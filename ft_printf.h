f5e8b4c1aec3a4a8b2103024c00ad1d65aaa0c97
Gohar Galstyan <gogalsty@c2r2s1.42yerevan.am>
2026-02-26 16:16:28 +0400

    PRINTF

--- ft_printf.h
+++ ft_printf.h
@@ -6,8 +6,25 @@
 /*   By: gogalsty <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 15:16:12 by gogalsty          #+#    #+#             */
-/*   Updated: 2026/02/14 15:16:16 by gogalsty         ###   ########.fr       */
+/*   Updated: 2026/02/26 16:13:11 by gogalsty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+size_t		ft_putchar(char c);
+size_t		ft_putstr(char *str);
+size_t		ft_putnbr_base(unsigned long num, char *base);
+size_t		ft_puthex(unsigned int n, int uppercase);
+size_t		ft_putptr(void *ptr);
+size_t		ft_handle_format(char c, va_list args);
+size_t		ft_putint(int n);
+size_t		ft_putunsigned(unsigned int n);
+int			ft_printf(const char *format, ...);
+
+#endif
